9f9180a43716384a61c8c0b0a320ed655783f630
marcosihe <marcosherrera.ib@gmail.com>
2021-10-11 03:14:04 -0300

    Estado inicial del proyecto

---
+++ src/components/gif-expert-app/GifExpertApp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from './GifExpertApp.module.scss'
+
+const GifExpertApp = () => {
+  return (
+    <div>
+      <h1 className={styles.title}>GifExpertApp</h1>
+      <hr />
+    </div>
+  );
+};
+
+export default GifExpertApp;
